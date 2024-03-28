9cacd3f819cbea09912914eb2ea9515b45be6daf
PGjun <pgj127@gmail.com>
2024-03-28 19:41:22 +0900

    qusdfj

--- discord_bot.py
+++ discord_bot.py
@@ -11,36 +11,29 @@ def check_new_posts_and_log():
     known_content = set()
     try:
         with open(log_file_path, 'r') as file:
-            for line in file.read().splitlines():
-                # 파일에서 게시물 번호만 추출하여 집합에 저장
-                known_content.add(line.split('|')[-1])  # 게시물 URL 마지막 부분이 번호
+            known_content.update(file.read().splitlines())
     except FileNotFoundError:
         pass
 
     base_url = "https://quasarzone.com"
-    url = f"{base_url}/bbs/qb_saleinfo"
-    response = requests.get(url)
+    response = requests.get(f"{base_url}/bbs/qb_saleinfo")
     soup = BeautifulSoup(response.text, 'html.parser')
-    posts = soup.find_all(class_='market-info-list')
+    posts = soup.find_all('div', class_='market-info-list')
 
     new_content_found = False
     for post in posts:
-        a_tag = post.find('a')
-        if a_tag and 'href' in a_tag.attrs:
-            post_url_suffix = a_tag.attrs['href']
-            post_url = base_url + post_url_suffix
-            post_id = post_url_suffix.split('/')[-1]
-            
+        post_link = post.select_one('.market-info-list-cont .tit a')
+        if post_link and 'href' in post_link.attrs:
+            post_url = base_url + post_link['href']
+            post_id = post_link['href'].split('/')[-1]
             if post_id not in known_content:
-                new_content_found = True
-                post_title = post.text.strip()
-                # 금액 정보 추출
-                price_info = post.find_next_sibling(class_='market-info-sub').find(class_='text-orange').text.strip()
+                title = post_link.select_one('.ellipsis-with-reply-cnt').text.strip()
+                price_info = post.select_one('.market-info-sub .text-orange').text.strip() if post.select_one('.market-info-sub .text-orange') else "가격 정보 없음"
 
                 # Embed 형태로 메시지 데이터를 구성합니다.
                 embed = {
-                    "title": "🔥 새로운 핫딜 🔥",
-                    "description": f"{post_title}\n금액: {price_info}",
+                    "title": title,
+                    "description": f"가격: {price_info}",
                     "color": 0xFF0000,
                     "fields": [
                         {
@@ -55,13 +48,12 @@ def check_new_posts_and_log():
                 payload = {"embeds": [embed]}
                 requests.post(DISCORD_WEBHOOK_URL, json=payload, headers=headers)
                 
-                # 중복 검사를 위해 게시물 제목과 URL을 함께 파일에 저장
                 known_content.add(post_id)
+                new_content_found = True
 
     if new_content_found:
         with open(log_file_path, 'w') as file:
-            for content in known_content:
-                file.write(f"{content}\n")
+            file.write("\n".join(known_content))
 
 # 스크립트 실행
 check_new_posts_and_log()
